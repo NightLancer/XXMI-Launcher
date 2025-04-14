03deedcf8e0a8ddaa4b00aa9f4ea9643972462f6
NightLancer <NightLancer@users.noreply.github.com>
2025-04-14 06:05:55 +0300

    fixed \ and '' errors in f-strings and restored system_info
    *done properly*
    [core\utils]

--- src/xxmi_launcher/core/utils/system_info.py
+++ src/xxmi_launcher/core/utils/system_info.py
@@ -5,19 +5,17 @@ from multiprocessing import Process, Array
 
 
 def is_wine():
-    #return any(x in os.environ for x in ['WINE', 'WINEPREFIX', 'WINELOADER'])
-    return False
+    return any(x in os.environ for x in ['WINE', 'WINEPREFIX', 'WINELOADER'])
 
 
 def log_system_info():
-    # try:
-        # process = SystemInfoProcess()
-        # process.start()
-        # process.join()
-        # logging.info(f'System: {process.data.raw.decode('utf-8')[:process.data.raw.find(b'\0')]}')
-    # except Exception as e:
-        # logging.exception(e)
-    logging.info('***System info***')
+    try:
+        process = SystemInfoProcess()
+        process.start()
+        process.join()
+        logging.info("System: {}".format(process.data.raw.decode('utf-8').split('\0', 1)[0]))
+    except Exception as e:
+        logging.exception(e)
 
 
 class SystemInfoProcess(Process):
@@ -28,35 +26,34 @@ class SystemInfoProcess(Process):
     def run(self):
         result = {}
 
-        # try:
-            # result['Platform'] = f'{os.name.upper()}{'+WINE' if is_wine() else ''}'
-        # except Exception:
-            # result['Platform'] = 'Unknown'
-        result['Platform'] = 'Windows'
+        try:
+            result['Platform'] = f"{os.name.upper()}{'+WINE' if is_wine() else ''}"
+        except Exception:
+            result['Platform'] = 'Unknown'
 
-        # try:
-            # import wmi
-            # pc = wmi.WMI()
-        # except Exception as e:
-            # import traceback
-            # error = f'WMI initialization failed on {result['Platform']} platform: {e}\n\n{traceback.format_exc()}'
-            # self.data.raw = error.encode('utf-8')
-            # return
+        try:
+            import wmi
+            pc = wmi.WMI()
+        except Exception as e:
+            import traceback
+            error = f"WMI initialization failed on {result['Platform']} platform: {e}\n\n{traceback.format_exc()}"
+            self.data.raw = error.encode('utf-8')
+            return
 
         try:
             os_info = pc.Win32_OperatingSystem()[0]
         except Exception:
             pass
 
-        # try:
-            # result['OS'] = f'{os_info.Name.encode('utf-8').split(b'|')[0].decode('utf-8')} {os_info.Version}'
-        # except Exception:
-        result['OS'] = 'Unknown'
+        try:
+            result['OS'] = f"{os_info.Name.encode('utf-8').split(b'|')[0].decode('utf-8')} {os_info.Version}"
+        except Exception:
+            result['OS'] = 'Unknown'
 
-        # try:
-            # result['CPU'] = pc.Win32_Processor()[0].Name.strip()
-        # except Exception:
-        result['CPU'] = 'Unknown'
+        try:
+            result['CPU'] = pc.Win32_Processor()[0].Name.strip()
+        except Exception:
+            result['CPU'] = 'Unknown'
 
         try:
             free_ram = round(float(os_info.FreePhysicalMemory) / 1048576, 2)
